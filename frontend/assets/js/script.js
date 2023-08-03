45f2099b488e72a4de6623a5070b73d65d57bdcc
Guil_NtC <guilberts48@gmail.com>
2023-08-03 14:20:32 -0300

    att

---
+++ frontend/assets/js/script.js
@@ -0,0 +1,8 @@
+    function menuShow() {
+         let menuMobile = document.querySelector('.menu-mobile')
+         if(menuMobile.classList.contains('open')){
+            menuMobile.classList.remove('open')
+         } else{
+            menuMobile.classList.add('open')
+         }
+    }
