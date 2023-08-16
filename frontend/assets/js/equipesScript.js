90a1c7fe47eee2ffd78ba5168e07a7bace02ebdd
Guilbert48 <guilberts48@gmail.com>
2023-08-16 12:13:05 -0300

    att

---
+++ frontend/assets/js/equipesScript.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.area', {duration: 1000});
+sr.reveal(
+'.area-2', 
+{
+    duration: 3000,
+    rotate:{ x:0, y: 80, z:0 }
+
+});
+sr.reveal(
+'.area-4', 
+{
+    duration: 3000,
+    rotate:{ x: 100, y: 0, z: 200 }
+
+});
--- frontend/assets/js/script.js
+++
@@ -1,8 +0,0 @@
-    function menuShow() {
-         let menuMobile = document.querySelector('.menu-mobile')
-         if(menuMobile.classList.contains('open')){
-            menuMobile.classList.remove('open')
-         } else{
-            menuMobile.classList.add('open')
-         }
-    }
